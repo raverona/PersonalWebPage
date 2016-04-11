0bb75e819d9e7de96a4c6bb145cae92e61239f4e
Rafael Verona <raverona@gmail.com>
2016-04-11 11:21:34 -0300

    Mudando o player para carregar com Javascript

---
+++ js/javascript.js
@@ -0,0 +1,21 @@
+var song_list = {
+	artist_1: "Blink-182", title_1: "I Miss You", path_1: "audio/Blink-182-IMissYou.mp3"
+};
+var curr_song = 1;
+
+function set_song_title(song_number) {
+	var song_title = song_list["title_" . curr_song];
+	var html_song_tile_placeholder = document.getElementById("song-title").innerHTML = song_title;
+}
+
+function set_artist_name(song_number) {
+	var song_title = song_list["artist_" . curr_song];
+	var html_song_tile_placeholder = document.getElementById("song-artist").innerHTML = song_title;
+}
+
+window.onload = function () {
+	document.getElementById("now-playing").src = "audio/Blink-182-IMissYou.mp3";
+	document.getElementById("player").load();
+	set_song_title(1);
+	set_artist_name(1);
+};
