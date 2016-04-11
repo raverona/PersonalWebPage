d5bb4b9242f3097b094665bdb8d44ada21779a98
Rafael Verona <raverona@hotmail.com>
2016-04-11 17:06:06 -0300

    Botões funcionando

--- js/javascript.js
+++ js/javascript.js
@@ -2,7 +2,8 @@ var song_list = {
 	artist_1: "Blink-182",					title_1: "I Miss You",				path_1: "audio/Blink-182-IMissYou.mp3",
 	artist_2: "Boys Like Girls",			title_2: "Two Is Better Than One",	path_2: "audio/BoysLikeGirls-TwoIsBetterThanOne.mp3"
 };
-var curr_song = 2;
+var curr_song_number = 2;
+var max_songs_number = 2;
 
 function set_song_title(song_number) {
 	var song_title = song_list["title_" + song_number];
@@ -14,17 +15,40 @@ function set_artist_name(song_number) {
 	document.getElementById("song-artist").innerHTML = song_title;
 }
 
+function load_song(song_number) {
+	document.getElementById("now-playing").src = song_list["path_" + song_number];
+	document.getElementById("player").load();
+	set_song_title(song_number);
+	set_artist_name(song_number);
+}
+
 function next_song() {
-	
+	if (curr_song_number == max_songs_number) {
+		curr_song_number = 1;
+	}
+	else {
+		curr_song_number++;
+	}
+	load_song(curr_song_number);
 }
 
 function previous_song() {
-
+	if (curr_song_number == 1) {
+		curr_song_number = max_songs_number;
+	}
+	else {
+		curr_song_number--;
+	}
+	load_song(curr_song_number);
 }
 
 window.onload = function () {
-	document.getElementById("now-playing").src = song_list["path_" + curr_song];
-	document.getElementById("player").load();
-	set_song_title(curr_song);
-	set_artist_name(curr_song);
+	load_song(curr_song_number);
+	$("#previous-button").click(function() {
+		previous_song();
+	});
+
+	$("#next-button").click(function() {
+		next_song();
+	});
 };
