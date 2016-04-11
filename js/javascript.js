ba9af05f2d361fe54b29e4a5155e2651643c2ccb
Rafael Verona <raverona@hotmail.com>
2016-04-11 14:46:42 -0300

    Criação dos botões para avançar e retroceder músicas

--- js/javascript.js
+++ js/javascript.js
@@ -1,21 +1,30 @@
 var song_list = {
-	artist_1: "Blink-182", title_1: "I Miss You", path_1: "audio/Blink-182-IMissYou.mp3"
+	artist_1: "Blink-182",					title_1: "I Miss You",				path_1: "audio/Blink-182-IMissYou.mp3",
+	artist_2: "Boys Like Girls",			title_2: "Two Is Better Than One",	path_2: "audio/BoysLikeGirls-TwoIsBetterThanOne.mp3"
 };
-var curr_song = 1;
+var curr_song = 2;
 
 function set_song_title(song_number) {
 	var song_title = song_list["title_" + song_number];
-	var html_song_tile_placeholder = document.getElementById("song-title").innerHTML = song_title;
+	document.getElementById("song-title").innerHTML = song_title;
 }
 
 function set_artist_name(song_number) {
 	var song_title = song_list["artist_" + song_number];
-	var html_song_tile_placeholder = document.getElementById("song-artist").innerHTML = song_title;
+	document.getElementById("song-artist").innerHTML = song_title;
+}
+
+function next_song() {
+
+}
+
+function previous_song() {
+
 }
 
 window.onload = function () {
-	document.getElementById("now-playing").src = "audio/Blink-182-IMissYou.mp3";
+	document.getElementById("now-playing").src = song_list["path_" + curr_song];
 	document.getElementById("player").load();
-	set_song_title(1);
-	set_artist_name(1);
+	set_song_title(curr_song);
+	set_artist_name(curr_song);
 };
