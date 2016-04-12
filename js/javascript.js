55bd7760a97abffe9a568f91a8e9efd75c009b06
Rafael Verona <raverona@hotmail.com>
2016-04-11 21:40:33 -0300

    Nova música e eliminação de variáveis redundantes

--- js/javascript.js
+++ js/javascript.js
@@ -1,19 +1,18 @@
 var song_list = {
 	artist_1: "Blink-182",					title_1: "I Miss You",				path_1: "audio/Blink-182-IMissYou.mp3",
-	artist_2: "Boys Like Girls",			title_2: "Two Is Better Than One",	path_2: "audio/BoysLikeGirls-TwoIsBetterThanOne.mp3"
-}
+	artist_2: "Boys Like Girls",			title_2: "Two Is Better Than One",	path_2: "audio/BoysLikeGirls-TwoIsBetterThanOne.mp3",
+	artist_3: "Syd Matters",				title_3: "Obstacles",				path_3: "audio/SydMatters-Obstacles.mp3"
+};
 
 var curr_song_number = 1;
-var max_songs_number = 2;
+var max_songs_number = 3;
 
 function set_song_title(song_number) {
-	var song_title = song_list["title_" + song_number];
-	document.getElementById("song-title").innerHTML = song_title;
+	document.getElementById("song-title").innerHTML = song_list["title_" + song_number];
 }
 
 function set_artist_name(song_number) {
-	var song_title = song_list["artist_" + song_number];
-	document.getElementById("song-artist").innerHTML = song_title;
+	document.getElementById("song-artist").innerHTML = song_list["artist_" + song_number];
 }
 
 function load_song(song_number) {
